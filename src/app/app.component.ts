badcb7a88892ee26352ccaf36a314481bf9e4a20
ghribiachref <achref.ghribi@esprit.tn>
2025-11-08 17:27:21 +0100

    Initial commit: FAYLA project setup

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [HeaderComponent, FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent { }
